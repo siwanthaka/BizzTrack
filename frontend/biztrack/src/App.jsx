b983ac7ac85871dcf4be344640b0b8a332fd4b0e
Sashika Dilmina <sashikadilmina01exed@gmail.com>
2026-01-10 12:11:47 +0530

    Defining App Routes and  Building the Login Form UI & Building the Sign-Up Form UI

--- frontend/biztrack/src/App.jsx
+++ frontend/biztrack/src/App.jsx
@@ -1,9 +1,58 @@
 import React from 'react'
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+import Expense from './pages/Dashboard/Expense';
+import UserProvider from './context/UserContext';
+import {Toaster} from 'react-hot-toast';
+
+const App = () => {
   return (
-    <div>App</div>
+    <UserProvider>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root/>}/>
+          <Route path= "/login" exact element={<Login/>}/>
+          <Route path= "/signUp" exact element={<SignUp/>}/>
+          <Route path= "/dashboard" exact element={<Home/>}/>
+          <Route path= "/income" exact element={<Income/>}/>
+          <Route path= "/expense" exact element={<Expense/>}/>
+        </Routes>
+      </Router>
+    </div>
+
+    <Toaster 
+      toastOptions={{
+        className: "",
+        style: {
+          fontsize: '13px'
+        },
+      }}
+      />
+
+    </UserProvider>
   )
 }
 
 export default App
+
+const Root = () => {
+  //check if token exists in localStorage
+  const isAuthenticated = !!!localStorage.getItem("token");
+
+  // Redirect to dashboard if authenticated, otherwise to login
+  return isAuthenticated ? ( 
+    <Navigate to= "/dashboard" />
+  ): (
+    <Navigate to= "/login" />
+  );
+};
--- frontend/biztrack/src/pages/Auth/Login.jsx
+++ frontend/biztrack/src/pages/Auth/Login.jsx
@@ -1,8 +1,116 @@
-import React from 'react'
+import React, { useState,useContext } from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import { Link,useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
+import { validateEmail } from '../../utils/helper';
+import axiosInstance from '../../utils/axiosinstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { UserContext } from '../../context/UserContext';
+
+
 
 const Login = () => {
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [error, seterror] = useState(null);
+
+  const { updateUser} = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  // Handle Login Form Submit
+  const handleLogin = async (e) =>{
+     e.preventDefault();
+
+    if (!validateEmail(email)) {
+      seterror("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      seterror("Please enter the password");
+      return;
+    }
+
+    seterror("");
+
+    // Login API Call
+    try {
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password,
+      });
+      const {token, user } = response.data;
+
+      if(token) {
+        localStorage.setItem("token", token);
+        updateUser(user)
+        navigate("/dashboard");
+      }
+    }catch (error) {
+      if (error.response && error.response.data.message) {
+        seterror(error.response.data.message);
+    } else{
+        seterror("Something went wrong. Pleasr try again.")
+}
+    }
+
+  }
+
+
   return (
-    <div>Login</div>
+    <AuthLayout>
+      <div className='lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center'>
+        <h3 className='text-lg font-semibold text-black'>Welcome Back</h3>
+        <p className='text-sm text-slate-700 mt-[5px] mb-6'>
+          Please enter your details to log in
+        </p>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({target}) => setEmail(target.value)}
+            label="Email Address"
+            placeholder="Sashika@example.com"
+            type="text"
+            />
+
+             <Input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            label="Password"
+            placeholder="Min 8 Character"
+            type="password"
+          />
+
+          
+          {error && 
+            <p className="text-red-500 text-sm font-medium">{error}</p>}
+          
+          <button
+            type="submit"
+            className="btn-primary"
+          >
+            LOGIN
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don’t have an account?{" "}
+            <Link
+              className="font-medium text-primary underline"
+              to="/signup"
+            >
+              Sign up
+            </Link>
+            </p>
+
+        </form>
+
+
+
+
+      </div>
+    </AuthLayout>
   )
 }
 
